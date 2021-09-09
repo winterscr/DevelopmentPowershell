803765f9c1ccf6884e8c158a85eb53e96cc80b56
Cara Winters <cara@altitudeangel.com>
2021-09-09 12:20:56 +0100

    Added New-Project cmdlet

--- Development/Development.psm1
+++ Development/Development.psm1
@@ -5,7 +5,9 @@
 .(Join-Path $PSScriptRoot 'functions/AzureDevOps.ps1')
 .(Join-Path $PSScriptRoot 'functions/Dotnet.ps1')
 .(Join-Path $PSScriptRoot 'functions/Kubernetes.ps1')
+.(Join-Path $PSScriptRoot 'functions/New-Project.ps1')
 
 Export-ModuleMember Remove-BuildFiles, Get-GitIgnore, Update-EventGrid, Start-Portainer, `
     Show-OutdatedReport, Remove-StaleBranches, ConvertTo-Base64, ConvertFrom-Base64, Edit-UserSecrets, `
-    Set-Title, Copy-CurrentPath, Edit-Hosts, Use-MiniKube, Use-DockerDesktop, Install-AzureCli
+    Set-Title, Copy-CurrentPath, Edit-Hosts, Use-MiniKube, Use-DockerDesktop, Install-AzureCli, `
+    New-Project
---
+++ Development/functions/New-Project.ps1
@@ -0,0 +1,33 @@
+function New-Project([string] $SolutionName, [string] $ProjectName, [string] $ProjectType = "console", [switch] $GitInit = $false, [switch] $GitCommit = $false)
+{
+    if($GitInit) {
+        git init
+        git branch -m main
+    }
+
+    Get-GitIgnore VisualStudio
+    "* text=auto eol=crlf" > .gitattributes
+    mkdir src/app
+    mkdir src/env
+    mkdir docs
+
+    "# $($ProjectName) Readme" > README.md
+    "# $($ProjectName) Documentation" > docs/README.md
+    "# $($ProjectName) Environment Files" > src/env/README.md
+
+    cd src/app
+    dotnet new tool-manifest
+    dotnet tool install paket
+    dotnet paket init && dotnet paket install && dotnet paket restore
+
+    dotnet new sln --name $SolutionName
+    dotnet new $ProjectType --name $ProjectName
+    dotnet sln add $ProjectName
+
+    cd ../..
+    git add -A
+
+    if($GitCommit) {
+        git commit -m "Initial commit"
+    }
+}
