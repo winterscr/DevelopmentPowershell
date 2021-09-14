eed03125951cdeb8807f481f19e50893b6cab506
Cara Winters <cara@altitudeangel.com>
2021-09-14 16:17:48 +0100

    Added SQL server docker commands

--- Development/Development.psm1
+++ Development/Development.psm1
@@ -6,8 +6,9 @@
 .(Join-Path $PSScriptRoot 'functions/Dotnet.ps1')
 .(Join-Path $PSScriptRoot 'functions/Kubernetes.ps1')
 .(Join-Path $PSScriptRoot 'functions/New-Project.ps1')
+.(Join-Path $PSScriptRoot 'functions/New-SqlServerDocker.ps1')
 
 Export-ModuleMember Remove-BuildFiles, Get-GitIgnore, Update-EventGrid, Start-Portainer, `
     Show-OutdatedReport, Remove-StaleBranches, ConvertTo-Base64, ConvertFrom-Base64, Edit-UserSecrets, `
     Set-Title, Copy-CurrentPath, Edit-Hosts, Use-MiniKube, Use-DockerDesktop, Install-AzureCli, `
-    New-Project
+    New-Project, New-SqlServerDocker, Remove-SqlServerDocker
---
+++ Development/functions/New-SqlServerDocker.ps1
@@ -0,0 +1,28 @@
+function New-SqlServerDocker([string] $Name, [int] $Port = 1435, [switch] $CreateVolume, [parameter(Mandatory = $false)][securestring] $Password)
+{
+    Write-Host "Creating SQL server instance $Name"
+    if (!$null)
+    {
+        $Password = ConvertTo-SecureString 'Pa$$w0rd' -AsPlainText
+    }
+    docker volume create $Name > $null
+    $createCommand = "docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=$(ConvertFrom-SecureString $Password -AsPlainText)' -p $($Port):1433 --name $($Name) " + `
+        "-h $($Name) -d -v $($Name):/var/opt/mssql mcr.microsoft.com/mssql/server:2019-latest"
+    
+    Invoke-Expression $createCommand > $null
+
+    Write-Host "SQL Server instance listening on port $Port"
+}
+
+function Remove-SqlServerDocker([string] $Name, [switch] $DeleteVolume)
+{
+    Write-Host "Stopping and removing container $Name"
+    docker container stop $Name > $null && docker container rm $Name > $null
+    if ($DeleteVolume)
+    {
+        Write-Host "Deleting volume $Name"
+        docker volume rm $Name > $null
+    }
+
+    Write-Host "Done"
+}
